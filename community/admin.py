dd4a49fc3f354423b76ad1e2a87f76ca0270325d
b <a>
2022-09-20 14:26:49 -0300

    infra-part1

--- community/admin.py
+++ community/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Question
+from .models import Community
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Community)
