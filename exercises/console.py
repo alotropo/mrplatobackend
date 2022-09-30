a2e87baa1893f1ac685351b5aac394449d26a6c8
b <a>
2022-09-29 23:41:16 -0300

    new functions

---
+++ exercises/console.py
@@ -0,0 +1,5 @@
+import glob, random
+file_path_type = ["./Memessdqwe/.png", "./Memes/.jpeg","ADSDASDADS"]
+images = random.choice(file_path_type)
+
+print(images)
--- exercises/models.py
+++ exercises/models.py
@@ -3,13 +3,24 @@ from django.db import models
 
 # Create your models here.
 
+DIFF_CHOICES = (
+    ('desafios', 'desafios'),
+    ('lista_exercicios', 'lista_exercicios'),
+    ('torneio', 'torneio'),
+)
+
+
 
 class Question(models.Model):
+    category = models.CharField(max_length=50)
     text = models.TextField()
     result = models.TextField()
-    category = models.CharField(max_length=50)
     data_created = models.DateField(auto_now=True)
     resolution = models.TextField()
+
+
+    def __str__(self) -> str:
+        return self.result
 
 
 class ListExercise(models.Model):
