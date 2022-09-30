f49de5050ff0aa7c4707d5516ef3f0fad1236f5e
b <a>
2022-09-30 17:31:31 -0300

    deploy test

--- core/settings/base.py
+++ core/settings/base.py
@@ -116,7 +116,7 @@ DATABASES = {
         'USER': "fhmkwviotlezwg",
         'PASSWORD':"82194719fbd4c6adc22ccee8675f28930ec0487c8b6fc30183cd65e99a36d109",
         'HOST':"ec2-34-200-205-45.compute-1.amazonaws.com",
-        # 'PORT':5432
+        # 'PORT':5432s
     }
 }
 
